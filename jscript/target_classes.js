147ed12a556fdcedcc1528e9846b22eed80e4ed2
harhspalod <harshspalod@gmail.com>
2024-11-01 21:38:33 +0530

    hello

---
+++ jscript/target_classes.js
@@ -0,0 +1,11 @@
+
+TARGET_CLASSES = {
+  0: 'Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
+  1: 'Basal Cell Carcinoma',
+  2: 'Benign Keratosis',
+  3: 'Dermatofibroma',
+  4: 'Melanoma',
+  5: 'Melanocytic Nevi',
+  6: 'Vascular skin lesion'
+
+};
